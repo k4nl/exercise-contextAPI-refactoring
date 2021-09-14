952343adf95a19390b4fa825673dfe9a85ccae0e
k4nl <gustavoh.bh@gmail.com>
2021-09-13 21:18:09 -0300

    x

--- src/TrafficSignal.jsx
+++ src/TrafficSignal.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 
 import CarsContext from './context/CarsContext';
 
-=======
-// src/TrafficSignal.jsx
-
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { changeSignal } from './redux/actionCreators';
->>>>>>> d767d69cfbc747526e347b0dce69183ca7a9fc1b
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
@@ -22,7 +13,6 @@ const renderSignal = (signalColor) => {
   return null;
 };
 
-<<<<<<< HEAD
 class TrafficSignal extends Component {
   render() {
     const { color } = this.context.signal;
@@ -49,36 +39,3 @@ class TrafficSignal extends Component {
 TrafficSignal.contextType = CarsContext;
 
 export default TrafficSignal;
-=======
-const TrafficSignal = ({ signalColor, changeSignal }) => {
-  return (
-    <div>
-      <div className="button-container">
-        <button onClick={() => changeSignal('red')} type="button">
-          Red
-        </button>
-        <button onClick={() => changeSignal('yellow')} type="button">
-          Yellow
-        </button>
-        <button onClick={() => changeSignal('green')} type="button">
-          Green
-        </button>
-      </div>
-      <img className="signal" src={renderSignal(signalColor)} alt="" />
-    </div>
-  );
-};
-
-const mapStateToProps = (state) => ({
-  signalColor: state.trafficReducer.signal.color
-});
-
-const mapDispatchToProps = { changeSignal };
-
-TrafficSignal.propTypes = {
-  changeSignal: PropTypes.func.isRequired,
-  signalColor: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(TrafficSignal);
->>>>>>> d767d69cfbc747526e347b0dce69183ca7a9fc1b
